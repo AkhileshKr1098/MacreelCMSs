b75e7cbd6ffab90c7d49e7fbbe75e906a5748ab2
AkhileshHjp <tech.kr.akhi@gmail.com>
2024-07-17 19:01:58 +0530

    app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.macrellcms.app',
+  appName: 'MacreelCMS',
+  webDir: 'www'
+};
+
+export default config;
